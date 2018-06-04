2b5ddf214755680a9b53347d68e914405cc72e4b
Nemo <me@captnemo.in>
2018-06-04 15:57:52 +0530

    fix: Maintain old gauge variable

--- server.js
+++ server.js
@@ -5,6 +5,16 @@ const metrics = require('./index');
 
 pClient.collectDefaultMetrics({ timeout: 60000 });
 
+let usedGauge = new pClient.Gauge({
+    name: 'act_fup_usage_gigabytes_total',
+    help: 'ACT current usage in GB',
+});
+
+let totalGauge = new pClient.Gauge({
+    name: 'act_fup_max_gigabytes_total',
+    help: 'ACT FUP limit in GB',
+});
+
 const requestHandler = async (req, res) => {
     let date = new Date(Date.now()).toLocaleString();
     console.log(`${date}: ${req.url}`);
@@ -13,19 +23,11 @@ const requestHandler = async (req, res) => {
             let m = await metrics.getUsage();
             // TODO: Switch to the correct err, res pattern with promise
             if (m !== null) {
-                let usedGauge = new pClient.Gauge({
-                    name: 'act_fup_usage_gigabytes_total',
-                    help: 'ACT current usage in GB',
-                });
                 usedGauge.set(m.used);
-                let totalGauge = new pClient.Gauge({
-                    name: 'act_fup_max_gigabytes_total',
-                    help: 'ACT FUP limit in GB',
-                });
                 totalGauge.set(m.total);
-                let register = pClient.register;
-                res.setHeader('Content-Type', register.contentType);
-                res.end(register.metrics());
+
+                res.setHeader('Content-Type', pClient.register.contentType);
+                res.end(pClient.register.metrics());
             } else {
                 res.sendStatus(500);
                 res.end('Scrape failed');
