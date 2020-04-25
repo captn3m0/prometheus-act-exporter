aeb9d6fc1db005aaf7b6761f311e357c11adde5a
Nemo <me@captnemo.in>
2020-04-25 15:47:28 +0530

    Remove debug logging

--- init.sh
+++ init.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 export CHROME_REVISION=$(node -e "console.log(require('/usr/local/share/.config/yarn/global/node_modules/puppeteer/package.json')['puppeteer']['chromium_revision'])")
 export CHROME_BIN="/usr/local/share/.config/yarn/global/node_modules/puppeteer/.local-chromium/linux-$CHROME_REVISION/chrome-linux/chrome"
-env
-ls -lah $CHROME_BIN
-echo $CHROME_BIN
+
 /usr/local/bin/node server.js $@
